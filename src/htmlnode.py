94271ae13b5a6b1470c25e612e8b2a86e2ad532f
HubTakenIs <hubertaslol@gmail.com>
2026-04-05 23:02:01 +0100

    leaf nodes and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,25 @@ class HTMLNode():
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children:{self.children}, {self.props})"
 
+class LeafNode(HTMLNode):
+    def __init__(self,tag, value, props=None):
+        super().__init__(value=value, tag=tag,props=props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError
+        if not self.tag:
+            return f"{self.value}"
+        out = f"<{self.tag}"
+        if not self.props:
+            out += f">{self.value}</{self.tag}>"
+        else:
+            # finish this?
+            out1 = ""
+            for prop in self.props:
+                out1 += f' {prop}="{self.props[prop]}"'
+            out += f"{out1}>{self.value}</{self.tag}>"
+
+        return out
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.props})"
