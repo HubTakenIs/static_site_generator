88dd833459db8aed0520c19cd713bf6aef73baeb
HubTakenIs <hubertaslol@gmail.com>
2026-04-07 23:55:36 +0100

    placeholder tests no more

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -25,15 +25,36 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.value, "This is a text node")
 
     def test_bold(self):
-        pass
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold text node")
+
     def test_italic(self):
-        pass
+        node = TextNode("This is an italian text node", TextType.ITALIC) # funny joke haha
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is an italian text node")
+  
     def test_code(self):
-        pass
+        node = TextNode("This is a code block text node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a code block text node")
+
     def test_link(self):
-        pass
+        node = TextNode("This is a link text node", TextType.LINK, "www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link text node")
+        self.assertEqual(html_node.props, {"href":"www.google.com"})
+
     def test_image(self):
-        pass
+        node = TextNode("This is an image text node", TextType.IMAGE, "www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props,{"src":"www.google.com", "alt":"This is an image text node"})
 
 if __name__ == "__main__":
     unittest.main()
