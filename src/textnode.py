dbaa96764425ca789fff4c302bd094a51e8c3d8b
HubTakenIs <hubertaslol@gmail.com>
2026-04-08 23:25:28 +0100

    saving changes

--- src/textnode.py
+++ src/textnode.py
@@ -41,4 +41,20 @@ def text_node_to_html_node(text_node):
             raise Exception("Unrecognised Text Type")
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    pass
+    out = []
+
+    for old_node in old_nodes:
+        if old_node.text_type is not TextType.TEXT:
+            out.append(old_node)
+            continue
+        nodes = []
+        split = old_node.split(delimiter, 2)
+        if len(split) == 3:
+            nodes.append(
+                    TextNode(split[0], TextType.TEXT),
+                    TextNode(split[1], text_type),
+                    TextNode(split[2], TextType.Text),)
+            out.extend(nodes)
+        else:
+            raise expection("invalid markdown syntax")
+    return out
