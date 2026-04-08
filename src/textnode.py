c4bb434e0a91b60ffd741fca9431ab1e5f8e151c
HubTakenIs <hubertaslol@gmail.com>
2026-04-08 22:14:12 +0100

    saving changes

--- src/textnode.py
+++ src/textnode.py
@@ -39,3 +39,6 @@ def text_node_to_html_node(text_node):
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
             raise Exception("Unrecognised Text Type")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    pass
