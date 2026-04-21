f864fd75eeb5d4479e5f775998c1416d064876b0
HubTakenIs <hubertaslol@gmail.com>
2026-04-22 00:01:52 +0100

    removed split_node function from textnode file

--- src/textnode.py
+++ src/textnode.py
@@ -40,23 +40,4 @@ def text_node_to_html_node(text_node):
         case _:
             raise Exception("Unrecognised Text Type")
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    out = []
 
-    for old_node in old_nodes:
-        if old_node.text_type is not TextType.TEXT:
-            out.append(old_node)
-            continue
-        split_nodes = []
-        split = old_node.text.split(delimiter)
-        if len(split) % 2 == 0:
-            raise ValueError("invalid markdown, formatted section not closed")
-        for i in range(len(split)):
-            if split[i] == "":
-                continue
-            if i % 2 == 0:
-                split_nodes.append(TextNode(split[i], TextType.TEXT))
-            else:
-                split_nodes.append(TextNode(split[i], text_type))
-        out.extend(split_nodes)
-    return out
