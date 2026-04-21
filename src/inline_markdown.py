4836ad8dad1afb4feaa854fb414c5695453300c6
HubTakenIs <hubertaslol@gmail.com>
2026-04-21 23:51:46 +0100

    moving split_nodes_delimiter to own file

---
+++ src/inline_markdown.py
@@ -0,0 +1,26 @@
+
+from textnode import (
+        TextType,
+        TextNode,
+        )
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    out = []
+
+    for old_node in old_nodes:
+        if old_node.text_type is not TextType.TEXT:
+            out.append(old_node)
+            continue
+        split_nodes = []
+        split = old_node.text.split(delimiter)
+        if len(split) % 2 == 0:
+            raise ValueError("invalid markdown, formatted section not closed")
+        for i in range(len(split)):
+            if split[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(split[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(split[i], text_type))
+        out.extend(split_nodes)
+    return out
