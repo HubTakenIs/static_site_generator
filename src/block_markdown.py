9333efcab0e85dafe87d1a32dbc1862b5936da9f
HubTakenIs <hubertaslol@gmail.com>
2026-06-11 23:53:31 +0100

    I wrote 3 lines of code woah

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -20,7 +20,11 @@ def markdown_to_html_node(markdown):
     for block in blocks:
         block_type = block_to_block_type(block)
         if block_type == BlockType.HEADING:
-            pass
+            count = 0
+            for char in block:
+                if char == "#":
+                    count += 1
+
         elif block_type == BlockType.CODE:
             pass
         elif block_type == BlockType.ORDERED_LIST:
