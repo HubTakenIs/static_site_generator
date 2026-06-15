d87de4f573b363114ea7f16a7b692397e324b5c5
HubTakenIs <hubertaslol@gmail.com>
2026-06-15 18:01:21 +0100

    added copy static, will add logging later and re-test

--- src/main.py
+++ src/main.py
@@ -1,7 +1,38 @@
 from textnode import TextNode, TextType
-def main():
-    testnode = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(testnode)
+import os
+import shutil
+import logging
 
+logger = logging.getLogger(__name__)
+
+def main():
+    print(f"Hello world")
+    print("copy static")
+    copy_static
+
+def copy_static():
+    # copy all of static files to public
+    # check if path exists, create public if it doesn't. delete public and re-create it if it does
+    # copy all files and subdirectories to from static to public
+    # log paths.
+    public_path = os.path.abspath("./public/")
+    path = os.path.exists(public_path)
+    if not path:
+        os.mkdir(public_path)
+    else:
+        shutil.rmtree(public_path)
+
+    # list all files and loop through.
+    static_path = os.path.abspath("./static/")
+    static_dir = os.listdir(static_path)
+    for line in static_dir:
+        src = os.path.join(static_path,line)
+        destination = os.path.join(public_path,line)
+        is_file = os.path.isfile(src)
+        if is_file:
+
+            shutil.copy(src, destination)
+        else:
+            shutil.copytree(src, destination)
 
 main()
