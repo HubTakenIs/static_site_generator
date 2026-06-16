e39d61eebac5c04f9d70b57c411c641e73a2f0db
HubTakenIs <hubertaslol@gmail.com>
2026-06-16 23:44:42 +0100

    mix up

--- src/main.py
+++ src/main.py
@@ -3,27 +3,38 @@ import os
 import shutil
 import logging
 
-logger = logging.getLogger(__name__)
+
+
+
 
 def main():
-    print(f"Hello world")
-    print("copy static")
-    copy_static
+    print(f"main")
+    copy_static()
 
 def copy_static():
     # copy all of static files to public
     # check if path exists, create public if it doesn't. delete public and re-create it if it does
     # copy all files and subdirectories to from static to public
     # log paths.
+    logging.basicConfig(filename='myapp.log', level=logging.INFO)
+    logging.info("copy_static started")
     public_path = os.path.abspath("./public/")
+    logging.info(f"public_path : {public_path}")
     path = os.path.exists(public_path)
+    logging.info(f"public_path exists: {path}")
     if not path:
+        logging.info("no path found, creating directory.")
         os.mkdir(public_path)
     else:
+        logging.info("path found, deleting directory and recreating it.")
         shutil.rmtree(public_path)
+        os.mkdir(public_path)
+
 
     # list all files and loop through.
+    logging.info("list all files and loop through")
     static_path = os.path.abspath("./static/")
+    logging.info(f"static_path: {static_path}")
     static_dir = os.listdir(static_path)
     for line in static_dir:
         src = os.path.join(static_path,line)
